2a1b1304349ee376878b983128b2aa627aef0bf7
mike zhang <mike.zhang@dpworld.com>
2021-05-03 02:41:21 -0700

    autosave with eslint

--- https:/javascript.info/map-set.ts
+++ https:/javascript.info/map-set.ts
@@ -1,15 +1,13 @@
 // https://javascript.info/map-set
 
-
 let map = new Map();
 
-map.set('1', 'str1');   // a string key
-map.set(1, 'num1');     // a numeric key
-map.set(true, 'bool1'); // a boolean key
+map.set("1", "str1"); // a string key
+map.set(1, "num1"); // a numeric key
+map.set(true, "bool1"); // a boolean key
 
 map.get(1); /*?+*/
-map.get('1'); /*?+*/
+map.get("1"); /*?+*/
 map.get(true); /*?+*/
 
-
-
+const asd = () => {};
